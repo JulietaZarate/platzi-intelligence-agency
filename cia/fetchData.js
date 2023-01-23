92b8f738e63a4431ca545c6f745957ec874def85
JulietaZarate <julizarate9965@gmail.com>
2023-01-23 11:22:22 -0300

    First Version

---
+++ cia/fetchData.js
@@ -0,0 +1,52 @@
+let = $accordion = document.getElementById("accordion");
+
+let ciaData = null;
+
+fetch(
+    "https://app.scrapinghub.com/api/v2/datasets/kdybI1E79wW/download?format=json"
+)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data) => {
+        //Work with JSON data here
+        ciaData = data;
+        cards = ciaData.map(
+            (x, idx) => `
+        <div class = "card">
+        <div class = "card-header bg-dark" id="headings${idx}">
+            <h2 class="mb-0">
+                <button
+                class = "btn btn-outline-light"
+                type = "button"
+                data-toggle = "collapse"
+                data-target = "#collapse${idx}"
+                aria-expanded = "true"
+                aria-controls = "collapse${idx}"
+            >
+            ${x.title}
+                </button>
+            </h2>
+        </div>
+
+    <div 
+        id = "collapse${idx}"
+        class = "collapse"
+        aria-labelledby = "heading${idx}"
+        data-parent = "#accordion"
+    >
+     <div class = "card-body">
+        ${x.body}
+        <br>
+        <br>
+        <a href=${x.url} target="_blank"> Ver mas </a>
+     </div>
+    </div>
+    `);
+            for (i = 0; i < cards.lenght; i++){
+                $accordion.innerHTML += cards[i];
+            }
+    })
+        .catch((err)=> {
+            // Do something for an error here
+        });
